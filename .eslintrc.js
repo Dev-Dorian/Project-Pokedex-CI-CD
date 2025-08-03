25689488be0abe74cc325d376db73ae393563063
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-08-02 22:20:28 -0400

    Fix Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,59 @@
+const isFixMode = process.argv.includes("--fix");
+
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest/globals": true
-  },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  plugins: {
+    react: reactPlugin,
+    "env": {
+      "browser": true,
+      "es6": true,
+      "jest/globals": true,
+      "node": true
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
-  },
-  "plugins": [
-    "react", "jest"
-  ],
-  "rules": {
-    "indent": [
-      "error",
+    "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      ["eslint:all", "plugin:react/all"]
+    ],
+    "parserOptions": {
+      "ecmaFeatures": {
+        "jsx": true
+      },
+      "ecmaVersion": 2018,
+      "sourceType": "module"
+    },
+    "plugins": [
+      "react", "jest",
+
+    ],
+    "rules": {
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "indent": [
+        "error",
         2
       ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "eqeqeq": "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
-    ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
-    "no-console": "error",
-    "react/prop-types": 0
+      "linebreak-style": [
+        "error",
+        "unix"
+      ],
+      "quotes": [
+        "error",
+        "single"
+      ],
+      "semi": [
+        "error",
+        "never"
+      ],
+      "eqeqeq": "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": [
+        "error", "always"
+      ],
+      "arrow-spacing": [
+        "error", { "before": true, "after": true }
+      ],
+      "no-console": "warn",
+      "react/prop-types": 0
+    }
   }
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Deployment Pipeline](https://github.com/Dev-Dorian/Project-Pokedex-CI-CD/actions/workflows/pipeline.yml/badge.svg)](https://github.com/Dev-Dorian/Project-Pokedex-CI-CD/actions/workflows/pipeline.yml)
+
 # Full Stack open CI/CD
 
 This repository is used for the CI/CD module of the Full stack open course
