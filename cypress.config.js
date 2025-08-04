13b6cc3572a17da8f8a3c320055a6175c308d949
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-08-04 17:52:33 -0400

    Add E2E tests to deployment pipeline

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:5000',
+    specPattern: 'cypress/integration/**/*.js',
+    supportFile: 'cypress/integration/app.js',
+    fixturesFolder: 'cypress/fixtures',
+    video: false,
+    screenshotOnRunFailure: true,
+  },
+})
---
+++ cypress/integration/app.js
@@ -0,0 +1,7 @@
+/* global cy */
+describe('Pokedex', function () {
+  it('frontpage can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('pikachu')
+  })
+})
