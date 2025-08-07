b769fa9ae1824dc7926aad4d21dcd39c4ae6b0dc
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-08-06 22:48:54 -0400

    Install -D wait-on

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,12 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5000',
     defaultCommandTimeout: 10000,
+    setupNodeEvents(on, config) {
+      on('before:run', async (details) => {
+        // Wait for the server to be available before running tests
+        await require('wait-on')({ resources: ['http://localhost:5000'] });
+      });
+    },
     specPattern: 'cypress/integration/**/*.js',
     supportFile: 'cypress/integration/app.js',
     fixturesFolder: 'cypress/fixtures',
