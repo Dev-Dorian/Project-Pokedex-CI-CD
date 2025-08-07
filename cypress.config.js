37c4105af584dc7e779262026e1ce6f963acc02e
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-08-06 21:14:09 -0400

    Update Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,16 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5000',
+    baseUrl: 'http://localhost:8080',
     specPattern: 'cypress/integration/**/*.js',
     supportFile: 'cypress/integration/app.js',
     fixturesFolder: 'cypress/fixtures',
     video: false,
     screenshotOnRunFailure: true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require('@cypress/code-coverage/task')(on, config)
+      return config
+    }
   },
 })
