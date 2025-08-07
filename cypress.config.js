21390becc9a6e090e29008dc307cdef57bb04793
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-08-07 16:24:27 -0400

    Update Pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,13 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5000',
+    setupNodeEvents(on, config) {
+      on('before:run', async (details) => {
+        // Wait for the server to be available before running tests
+        await require('wait-on')({ resources: ['http://localhost:5000'] });
+      });
+    },
+    responseTimeout: 60000,
     specPattern: 'cypress/integration/**/*.js',
     supportFile: 'cypress/integration/app.js',
     fixturesFolder: 'cypress/fixtures',
