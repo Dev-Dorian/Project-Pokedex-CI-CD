cd9200f9c489a8109ced816580c5d62096f91270
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-08-06 22:38:45 -0400

    Update

--- cypress.config.js
+++ cypress.config.js
@@ -2,16 +2,11 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://localhost:5000',
     specPattern: 'cypress/integration/**/*.js',
     supportFile: 'cypress/integration/app.js',
     fixturesFolder: 'cypress/fixtures',
     video: false,
-    screenshotOnRunFailure: true,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      require('@cypress/code-coverage/task')(on, config)
-      return config
-    }
+    screenshotOnRunFailure: true
   },
 })
